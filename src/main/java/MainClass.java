58bf642099e9ed6089d5d5c86078dc99bc4f27dd
Apostolos Xenos <apostolosxenos@outlook.com>
2020-02-20 16:27:19 +0200

    Implemented FileWriter class to extract Json content to json files. They are located at files directory.

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,12 +1,14 @@
 import org.json.simple.JSONArray;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class MainClass {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         JReader jReaderOne = new JReader(new File("files/one.json"));
         JReader jReaderTwo = new JReader(new File("files/two.json"));
@@ -23,7 +25,14 @@ public class MainClass {
         Map<String, JSONArray> mapResultsOneAndThree = jReaderResults.getResultsForConfirmation("mergeOne&Three");
 
         Map<String, JSONArray> finalMap1 = mergeGames(map1, map2);
+        FileWriter fw = new FileWriter("files/OneAndTwo.json");
+        fw.write(finalMap1.toString());
+        fw.close();
         Map<String, JSONArray> finalMap2 = mergeGames(map1, map3);
+        fw = new FileWriter("files/OneAndThree.json");
+        fw.write(finalMap2.toString());
+        fw.close();
+
 
         // Comparison for confirmation
         if (compareResults(finalMap1, mapResultsOneAndTwo))
