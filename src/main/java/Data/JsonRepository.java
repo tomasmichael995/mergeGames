00b1da73cde51e41b1333d4880c66f7a2b9abc19
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2020-02-21 02:46:04 +0100

    Added constructor to JsonRepository

--- src/main/java/Data/JsonRepository.java
+++ src/main/java/Data/JsonRepository.java
@@ -1,9 +1,15 @@
 package Data;
 
 import POJOs.Game;
-import org.json.simple.JSONObject;
+
+import java.io.File;
 
 public class JsonRepository implements GameRepository {
+    private File file;
+
+    public JsonRepository(String filePath) {
+        file = new File(filePath);
+    }
 
     @Override
     public Game[] getGames() {
