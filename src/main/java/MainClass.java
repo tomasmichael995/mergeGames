a58adc721674b2666871666aadc887b98f20f19b
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2020-02-21 19:17:35 +0100

    Deleted redundant JsonAppender and implemented JsonRepository.mergeWithoutDuplicate()

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,14 +1,24 @@
 import Model.Game;
 import Persistance.GameRepository;
 import Persistance.Json.JsonRepository;
+import Persistance.MergeBehavior;
 
 import java.io.IOException;
 
 public class MainClass {
+    private static final String FILE_ONE = "files/one.json";
+    private static final String FILE_ONE_COPY = "files/oneCopy.json";
 
     public static void main(String[] args) throws IOException {
-        GameRepository one = new JsonRepository("files/one.json");
-        Game[] oneGames = one.loadGames();
-        one.save(oneGames, "files/oneCopy.json");
+        GameRepository rep1 = new JsonRepository(FILE_ONE);
+        Game[] games1 = rep1.loadGames();
+        rep1.save(games1, FILE_ONE_COPY);
+
+        GameRepository rep2 = new JsonRepository(FILE_ONE_COPY);
+        Game[] games2 = rep2.loadGames();
+
+        rep2.merge(games1, FILE_ONE_COPY, MergeBehavior.APPEND_ALL_GAMES);
+
+        System.out.println("Finished.");
     }
 }
--- src/main/java/Persistance/Json/JsonRepository.java
+++ src/main/java/Persistance/Json/JsonRepository.java
@@ -5,6 +5,9 @@ import Persistance.GameRepository;
 import Persistance.MergeBehavior;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public final class JsonRepository implements GameRepository {
     private final JsonReader reader;
@@ -42,10 +45,18 @@ public final class JsonRepository implements GameRepository {
     private void mergeAll(Game[] games, File file) {
         Game[] thisRepoGames = loadGames();
         Game[] all = new Game[thisRepoGames.length + games.length];
+
         JsonWriter writer = new JsonWriter(file);
         writer.write(games);
     }
 
     private void mergeWithoutDuplicate(Game[] games, File file) {
+        Game[] thisRepoGames = loadGames();
+        Game[] all = new Game[thisRepoGames.length + games.length];
+        Set<Game> uniqueGames = new HashSet<>();
+        uniqueGames.addAll(Arrays.asList(all));
+
+        JsonWriter writer = new JsonWriter(file);
+        writer.write(uniqueGames.toArray(new Game[uniqueGames.size()]));
     }
 }
