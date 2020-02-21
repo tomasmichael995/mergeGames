db19836ad26fe10eb5a3cd6bf6ec415d0d69e343
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2020-02-21 02:15:47 +0100

    Created implementation files for JsonReader/Parser

---
+++ src/main/java/JSON/JsonParserImp.java
@@ -0,0 +1,13 @@
+package JSON;
+
+import POJOs.Game;
+import org.json.simple.JSONObject;
+
+public class JsonParserImp implements JsonParser{
+
+    @Override
+    public Game ParseGameFrom(JSONObject gameJson) {
+        // TODO: implement
+        return null;
+    }
+}
---
+++ src/main/java/JSON/JsonReaderImp.java
@@ -0,0 +1,12 @@
+package JSON;
+
+import org.json.simple.JSONObject;
+
+public class JsonReaderImp implements JsonReader{
+
+    @Override
+    public JSONObject readJsonFrom(String file) {
+        // TODO: implement
+        return null;
+    }
+}
