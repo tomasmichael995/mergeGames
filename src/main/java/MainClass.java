3e26b1f9e20120e4a6ca3d6e45043943be718885
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2020-02-21 23:20:04 +0100

    Implemented operation in main()

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,31 +1,24 @@
 import Model.Game;
 import Persistance.GameRepository;
 import Persistance.Json.JsonRepository;
+import Persistance.SaveBehavior;
+import Utils.ArrayUtils;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainClass {
-    private static final String FILE_ONE = "files/one.json";
-    private static final String FILE_TWO = "files/two.json";
-    private static final String FILE_THREE = "files/three.json";
-    private static final String FILE_TEST = "files/test.json";
+    private static final GameRepository firstRep = new JsonRepository("files/one.json");
+    private static final GameRepository secondRep = new JsonRepository("files/two.json");
+    private static final GameRepository thirdRep = new JsonRepository("files/three.json");
+    private static final GameRepository resultRep = new JsonRepository("files/result.json");
 
     public static void main(String[] args) throws IOException {
-        GameRepository oneRep = new JsonRepository(FILE_ONE);
-        Game[] oneGames = oneRep.loadGames();
-
-        GameRepository testRep = new JsonRepository(FILE_TEST);
-        testRep.merge(oneGames);
-        Game[] testGames = testRep.loadGames();
-
-        // Sort first because Arrays.equals() is considering also elements position
-        // to calculate equality
-        Arrays.sort(oneGames);
-        Arrays.sort(testGames);
-        boolean identical = Arrays.equals(oneGames, testGames);
-
-        // Works if true!
-        System.out.println("Are the games identical? " + identical);
+        resultRep.save(firstRep.loadGames(), SaveBehavior.WRITE);
+        resultRep.merge(secondRep.loadGames());
+        resultRep.merge(thirdRep.loadGames());
+        System.out.println("Operation finished, check " + "files/result.json file.");
     }
 }
